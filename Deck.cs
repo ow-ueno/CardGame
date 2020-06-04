750a10fc445bddeb825b37f5c08778ea2415d331
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-04 10:29:50 +0900

    「JOKERNUM」を追加しました

--- Deck.cs
+++ Deck.cs
@@ -9,25 +9,30 @@ namespace CardGame
     {
         List<Card> Cards = new List<Card>();
 
+        //数字カード
         const int MAXNUM = 13;
-        const int SUITNUM = 4;
+        const int SUIT = 4;
+        //ジョーカー
+        const int JOKERNUM = 14;
+        //suitの名前
         List<string> CARDSUIT = new List<string> { "spade", "heart", "diamond", "clover", "red", "black" };
 
         public Deck()
         {
-            for (int i = 0; i < SUITNUM; i++)
+            for (int i = 0; i < SUIT; i++)
             {
                 for (int j = 1; j <= MAXNUM; j++)
                 {
                     //ただしいカードのつくりかた
+                    //コンストラクタで初期値を設定することで、以後変更できないようにする
                     var TmpCard = new Card(j, CARDSUIT[i]);
                     Cards.Add(TmpCard);
                 }
             }
-            //特殊なカードを2枚追加
-            var RedJoker = new Card(14, CARDSUIT[4]);
+            //ジョーカーを2枚追加
+            var RedJoker = new Card(JOKERNUM, CARDSUIT[4]);
             Cards.Add(RedJoker);
-            var BlackJoker = new Card(14, CARDSUIT[5]);
+            var BlackJoker = new Card(JOKERNUM, CARDSUIT[5]);
             Cards.Add(BlackJoker);
         }
 
